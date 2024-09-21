ce5f7fba5f93771b427eeac2484cafcec8431ddc
Selrach1107 <167187234+Selrach1107@users.noreply.github.com>
2024-09-22 07:13:32 +0800

    Update catlist.php

--- vendor/catlist.php
+++ vendor/catlist.php
@@ -12,8 +12,6 @@ include '../conn.php';
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
      <!-- Font Awesome for Icons -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-     <!-- Chart.js for Graphs -->
-     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
 
