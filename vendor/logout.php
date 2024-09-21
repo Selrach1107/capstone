a306cd5b3c60a8806240a12188d7f9249ec88710
Selrach1107 <167187234+Selrach1107@users.noreply.github.com>
2024-09-22 07:06:57 +0800

    Update logout.php

--- vendor/logout.php
+++ vendor/logout.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-// I-clear ang lahat ng session variables
+
 $_SESSION = array();
 
-// Tanggalin ang session cookie kung ito ay set
+
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -13,10 +13,10 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Destroy ang session
+
 session_destroy();
 
-// Redirect sa login page
+
 header("Location: login_form.php");
 exit();
 ?>
